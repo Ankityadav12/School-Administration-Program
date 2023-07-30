fa186d2f37863cdc64e51651c2ecdcbe5a5f972f
Ankit Yadav <51693899+Ankityadav12@users.noreply.github.com>
2023-07-30 16:34:01 +0530

    School Administration Program_Project_1.py

--- School Administration Program_Project_1.py
+++ School Administration Program_Project_1.py
@@ -2,7 +2,7 @@ import os
 import platform
 
 global listStd 
-listStd = ["Ankit ", "Vishanavi", "Siddesh", "Neha ", "kabir"] 
+listStd = ["Name1 ", "Name2", "Name3", "Name4 ", "Name5"] 
 
 def manageStudent(): 
 
